39e6898ff3ff0636e98566bdecf79d8eabdb9278
Elyter <eliottbreg@gmail.com>
2024-10-16 01:07:05 +0200

    Match and Terrain central working

--- api/server.js
+++ api/server.js
@@ -12,6 +12,9 @@ app.use(cors({
 
 app.use(express.json());
 
+// Définition des ressources (terrains)
+const resources = ['0', '1', '2', '3'];
+
 // Modifions le chemin du fichier DATA_FILE
 const DATA_FILE = path.join(__dirname, 'data.json');
 
@@ -31,39 +34,45 @@ app.post('/api/events', async (req, res) => {
     let events = JSON.parse(data);
     const newEvent = req.body;
 
-    // Vérification des chevauchements
-    const conflictingEvents = events.filter(event => 
-      event.resourceId === newEvent.resourceId &&
-      ((new Date(event.start) < new Date(newEvent.end) && new Date(event.end) > new Date(newEvent.start)))
-    );
-
-    if (conflictingEvents.length > 0) {
-      return res.status(400).json({ error: 'Un événement existe déjà sur ce terrain pour cette plage horaire' });
-    }
-
-    // Vérification pour resourceId 3
-    if (newEvent.resourceId === '3') {
+    if (newEvent.title === 'Match') {
       const conflictingEvents = events.filter(event => 
-        (event.resourceId === '1' || event.resourceId === '2') &&
         ((new Date(event.start) < new Date(newEvent.end) && new Date(event.end) > new Date(newEvent.start)))
       );
 
       if (conflictingEvents.length > 0) {
-        return res.status(400).json({ error: 'Les terrains 2 et 3 sont déjà réservés pour cette plage de temps' });
+        return res.status(400).json({ error: 'Un événement existe déjà sur un des terrains pour cette plage horaire' });
       }
 
-      // Modification de la couleur pour les événements "Indisponible"
-      const unavailableEvent1 = { ...newEvent, id: Date.now().toString(), title: 'Indisponible', description: '', resourceId: '1', color: '#808080' };
-      const unavailableEvent2 = { ...newEvent, id: (Date.now() + 1).toString(), title: 'Indisponible', description: '', resourceId: '2', color: '#808080' };
-      events.push(unavailableEvent1, unavailableEvent2);
+      const matchEvents = resources.map(resourceId => ({
+        ...newEvent,
+        id: Date.now().toString() + resourceId,
+        resourceId: resourceId,
+      }));
+
+      events.push(...matchEvents);
+    } else {
+      if (newEvent.resourceId === '3') {
+        const conflictingEvents = events.filter(event => 
+          (event.resourceId === '1' || event.resourceId === '2') &&
+          ((new Date(event.start) < new Date(newEvent.end) && new Date(event.end) > new Date(newEvent.start)))
+        );
+      
+        if (conflictingEvents.length > 0) {
+          return res.status(400).json({ error: 'Les terrains 2 et/ou 3 sont déjà réservés pour cette plage de temps' });
+        }
+      
+        const unavailableEvent1 = { ...newEvent, id: Date.now().toString(), title: 'Indisponible', description: '', resourceId: '1', color: '#808080' };
+        const unavailableEvent2 = { ...newEvent, id: (Date.now() + 1).toString(), title: 'Indisponible', description: '', resourceId: '2', color: '#808080' };
+        events.push(unavailableEvent1, unavailableEvent2);
+      }
+      // Assurez-vous que newEvent a une couleur
+      if (!newEvent.color) {
+        newEvent.color = '#3174ad'; // Couleur par défaut si non spécifiée
+      }
+  
+      events.push(newEvent);
     }
 
-    // Assurez-vous que newEvent a une couleur
-    if (!newEvent.color) {
-      newEvent.color = '#3174ad'; // Couleur par défaut si non spécifiée
-    }
-
-    events.push(newEvent);
     await fs.writeFile(DATA_FILE, JSON.stringify(events, null, 2));
     res.status(201).json(newEvent);
   } catch (error) {
@@ -84,7 +93,61 @@ app.put('/api/events/:id', async (req, res) => {
       return res.status(404).json({ error: 'Événement non trouvé' });
     }
 
-    events[eventIndex] = { ...events[eventIndex], ...updatedEvent };
+    const originalEvent = events[eventIndex];
+
+    if (originalEvent.title === 'Match') {
+      // Vérifier les chevauchements pour tous les terrains, en excluant les autres événements de match
+      const conflictingEvents = events.filter(event => 
+        event.title !== 'Match' &&
+        ((new Date(event.start) < new Date(updatedEvent.end) && new Date(event.end) > new Date(updatedEvent.start)))
+      );
+
+      if (conflictingEvents.length > 0) {
+        return res.status(400).json({ error: 'La modification chevauche des réservations existantes non-match' });
+      }
+
+      // Mettre à jour tous les événements du match
+      events = events.map(event => {
+        if (event.start === originalEvent.start && event.end === originalEvent.end && event.title === 'Match') {
+          return { ...event, ...updatedEvent, id: event.id, resourceId: event.resourceId };
+        }
+        return event;
+      });
+    } else {
+      if (updatedEvent.resourceId === '3') {
+        const conflictingEvents = events.filter(event => 
+          event.id !== eventId && // Exclure l'événement en cours de modification
+          (event.resourceId === '1' || event.resourceId === '2') &&
+          event.title !== 'Indisponible' && // Exclure les événements "Indisponible" associés
+          ((new Date(event.start) < new Date(updatedEvent.end) && new Date(event.end) > new Date(updatedEvent.start)))
+        );
+      
+        if (conflictingEvents.length > 0) {
+          return res.status(400).json({ error: 'Les terrains 2 et/ou 3 sont déjà réservés pour cette plage de temps' });
+        }
+
+        // Mettre à jour les événements "Indisponible" associés
+        events = events.map(event => {
+          if (event.start === originalEvent.start && event.end === originalEvent.end && event.title === 'Indisponible' && (event.resourceId === '1' || event.resourceId === '2')) {
+            return { ...event, start: updatedEvent.start, end: updatedEvent.end };
+          }
+          return event;
+        });
+      } else {
+        // Vérification des conflits pour les autres terrains
+        const conflictingEvents = events.filter(event => 
+          event.id !== eventId &&
+          event.resourceId === updatedEvent.resourceId &&
+          ((new Date(event.start) < new Date(updatedEvent.end) && new Date(event.end) > new Date(updatedEvent.start)))
+        );
+
+        if (conflictingEvents.length > 0) {
+          return res.status(400).json({ error: 'La modification chevauche des réservations existantes' });
+        }
+      }
+
+      events[eventIndex] = { ...events[eventIndex], ...updatedEvent };
+    }
 
     await fs.writeFile(DATA_FILE, JSON.stringify(events, null, 2));
     res.json(events[eventIndex]);
@@ -100,15 +163,34 @@ app.delete('/api/events/:id', async (req, res) => {
     let events = JSON.parse(data);
     const eventId = req.params.id;
 
-    const eventIndex = events.findIndex(event => event.id === eventId);
-    if (eventIndex === -1) {
+    const eventToDelete = events.find(event => event.id === eventId);
+    if (!eventToDelete) {
       return res.status(404).json({ error: 'Événement non trouvé' });
     }
 
-    events.splice(eventIndex, 1);
+    if (eventToDelete.title === 'Match') {
+      // Supprimer tous les événements du match
+      events = events.filter(event => 
+        !(event.start === eventToDelete.start && 
+          event.end === eventToDelete.end && 
+          event.title === 'Match')
+      );
+    } else if (eventToDelete.resourceId === '3') {
+      // Supprimer l'événement spécifié et les réservations "Indisponible" associées
+      events = events.filter(event => 
+        !(event.id === eventId || 
+          (event.start === eventToDelete.start && 
+           event.end === eventToDelete.end && 
+           event.title === 'Indisponible' && 
+           (event.resourceId === '1' || event.resourceId === '2')))
+      );
+    } else {
+      // Supprimer uniquement l'événement spécifié
+      events = events.filter(event => event.id !== eventId);
+    }
 
     await fs.writeFile(DATA_FILE, JSON.stringify(events, null, 2));
-    res.json({ message: 'Événement supprimé avec succès' });
+    res.json({ message: 'Événement(s) supprimé(s) avec succès' });
   } catch (error) {
     console.error('Erreur lors de la suppression de l\'événement:', error);
     res.status(500).json({ error: 'Erreur lors de la suppression de l\'événement' });
